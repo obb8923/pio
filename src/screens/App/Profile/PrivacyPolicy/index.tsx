b608e7440921b8b12dc5224336a046d1bb45471a
obb8923 <obb8923@naver.com>
2025-06-08 13:42:10 +0900

    개인정보,이용약관에 앱바추가

--- src/screens/App/Profile/PrivacyPolicy/index.tsx
+++ src/screens/App/Profile/PrivacyPolicy/index.tsx
@@ -1,10 +1,14 @@
 import { View, Text, ScrollView } from "react-native"
 import { Background } from "../../../../components/Background"
 import { Privacy } from "../../../../constants/PrivacyAndPolicy"
-
-export const PrivacyPolicyScreen = () => {
+import { AppBar } from "../../../../components/AppBar"
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import { ProfileStackParamList } from "../../../../nav/stack/Profile"
+type PrivacyPolicyScreenProps = NativeStackScreenProps<ProfileStackParamList, 'PrivacyPolicy'>;
+export const PrivacyPolicyScreen = ({navigation}:PrivacyPolicyScreenProps) => {
     return (
-        <Background isStatusBarGap={false} type="background">
+        <Background isStatusBarGap={true} type="background">
+            <AppBar navigation={navigation} />
             <ScrollView className="flex-1 p-4"
             contentContainerStyle={{paddingBottom: 50}}
             >
--- src/screens/App/Profile/TermsOfService/index.tsx
+++ src/screens/App/Profile/TermsOfService/index.tsx
@@ -1,10 +1,16 @@
 import { View, Text, ScrollView } from "react-native"
 import { Background } from "../../../../components/Background"
 import { Policy } from "../../../../constants/PrivacyAndPolicy"
-
-export const TermsOfServiceScreen = () => {
+import { AppBar } from "../../../../components/AppBar"
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import { ProfileStackParamList } from "../../../../nav/stack/Profile"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+type TermsOfServiceScreenProps = NativeStackScreenProps<ProfileStackParamList, 'TermsOfService'>;
+export const TermsOfServiceScreen = ({navigation}:TermsOfServiceScreenProps) => {
+    const insets = useSafeAreaInsets();
     return (
-        <Background isStatusBarGap={false} type="background">
+        <Background isStatusBarGap={true} type="background">
+            <AppBar navigation={navigation} />
             <ScrollView className="flex-1 p-4"
             contentContainerStyle={{paddingBottom: 50}}
             >
