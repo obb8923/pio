7df9ba32dbac3ab33b75625454155a1d0daab1ed
obb8923 <obb8923@naver.com>
2025-06-08 13:52:11 +0900

    .

--- src/screens/App/Profile/TermsOfService/index.tsx
+++ src/screens/App/Profile/TermsOfService/index.tsx
@@ -4,10 +4,8 @@ import { Policy } from "../../../../constants/PrivacyAndPolicy"
 import { AppBar } from "../../../../components/AppBar"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { ProfileStackParamList } from "../../../../nav/stack/Profile"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
 type TermsOfServiceScreenProps = NativeStackScreenProps<ProfileStackParamList, 'TermsOfService'>;
 export const TermsOfServiceScreen = ({navigation}:TermsOfServiceScreenProps) => {
-    const insets = useSafeAreaInsets();
     return (
         <Background isStatusBarGap={true} type="background">
             <AppBar navigation={navigation} />
