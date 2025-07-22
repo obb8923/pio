2d769ff49b6b53eb29fedb7fc987b408c413b36f
obb8923 <obb8923@naver.com>
2025-07-22 15:31:01 +0900

    fix: ios에서 textInput이 키보드에 가려지는 것 수정

--- src/screens/App/Map/ImageProcessing/components/DescriptionModal.tsx
+++ src/screens/App/Map/ImageProcessing/components/DescriptionModal.tsx
@@ -1,4 +1,4 @@
-import { View, TextInput, Modal, Text} from "react-native";
+import { View, TextInput, Modal, Text, Platform} from "react-native";
 import { CustomButton } from "../../../../../components/CustomButton";
 import { useModalBackground } from '../../../../../libs/hooks/useModalBackground';
 import { DEVICE_HEIGHT } from "../../../../../constants/normal";
@@ -15,7 +15,18 @@ export const DescriptionModal = ({
   description, 
   onDescriptionChange 
 }: DescriptionModalProps) => {
+    
     useModalBackground(isVisible);
+
+    const containerStyle = Platform.select({
+      ios: {
+        height: DEVICE_HEIGHT / 2,
+      },
+      android: {
+        flex: 1,
+      },
+    });
+
   return (
     <Modal
       visible={isVisible}
@@ -23,7 +34,10 @@ export const DescriptionModal = ({
       transparent={true}
     >
         {/* 내부 영역*/}
-        <View className="flex-1 relative mx-4 mt-20 mb-16 pt-4 pb-2 border border-greenTab900 rounded-3xl bg-greenTab">
+        <View 
+        className=" relative mx-4 mt-20 mb-16 pt-4 pb-2 border border-greenTab900 rounded-3xl bg-greenTab"
+        style={containerStyle}
+        >
             {/* 타이틀 */}
           <View className="items-start justify-end px-4 py-2" style={{height: DEVICE_HEIGHT/18}}>
             <Text className="text-center text-lg text-greenActive">식물에 대한 설명을 입력해주세요</Text>
--- src/screens/App/Map/ImageProcessing/components/MemoModal.tsx
+++ src/screens/App/Map/ImageProcessing/components/MemoModal.tsx
@@ -1,6 +1,7 @@
-import { View, TextInput, Modal, Text, Dimensions } from "react-native";
+import { View, TextInput, Modal, Text, Platform } from "react-native";
 import { CustomButton } from "../../../../../components/CustomButton";
 import { useModalBackground } from '../../../../../libs/hooks/useModalBackground';
+import { DEVICE_HEIGHT } from "../../../../../constants/normal";
 
 interface MemoModalProps {
   isVisible: boolean;
@@ -15,8 +16,17 @@ export const MemoModal = ({
   memo, 
   onMemoChange 
 }: MemoModalProps) => {
-  const height = Dimensions.get('window').height;
+
   useModalBackground(isVisible);
+
+  const containerStyle = Platform.select({
+    ios: {
+      height: DEVICE_HEIGHT / 2,
+    },
+    android: {
+      flex: 1,
+    },
+  });
   return (
     <Modal
       visible={isVisible}
@@ -25,9 +35,11 @@ export const MemoModal = ({
     >
    
         {/* 내부 영역*/}
-        <View className="flex-1 relative mx-4 mt-20 mb-16 pt-4 pb-2 border border-greenTab900 rounded-3xl bg-greenTab">
+        <View 
+        className=" relative mx-4 mt-20 mb-16 pt-4 pb-2 border border-greenTab900 rounded-3xl bg-greenTab"
+        style={containerStyle}>
           {/* 타이틀 */}
-          <View className="items-start justify-end px-4 py-2" style={{height: height/18}}>
+          <View className="items-start justify-end px-4 py-2" style={{height: DEVICE_HEIGHT/18}}>
             <Text className="text-center text-lg text-greenActive">식물에 대한 메모를 입력해주세요</Text>
           </View>
           {/* 텍스트 인풋 */}
@@ -40,8 +52,8 @@ export const MemoModal = ({
             autoFocus
           />
           {/* 버튼 */}
-          <View className="flex-row justify-end items-center px-4" style={{height: height/13}}>
-            <CustomButton text="완료" size={height/15} onPress={onClose}/>
+          <View className="flex-row justify-end items-center px-4" style={{height: DEVICE_HEIGHT/13}}>
+            <CustomButton text="완료" size={DEVICE_HEIGHT/15} onPress={onClose}/>
           </View>
         </View>
     </Modal>
