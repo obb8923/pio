da6e0b034ccd2b89c3d743a8fc79934e08b3f88c
obb8923 <obb8923@naver.com>
2025-07-29 20:44:07 +0900

    온보딩 틀

--- src/screens/Onboarding/Onboarding1.tsx
+++ src/screens/Onboarding/Onboarding1.tsx
@@ -1,4 +1,18 @@
+import {Background} from '../../components/Background'
+import {View,TouchableOpacity} from 'react-native'
 export const Onboarding1Screen = ()=>{
-    return (<>
-    </>)
+    return (
+        <Background isStatusBarGap={true} isTabBarGap={false}>
+        <View className="flex-1 p-4 justify-end items-center">
+            {/* ui animation section */}
+           <View className="w-full h-5/6 bg-black"></View>
+           {/* button section */}
+           <View className="w-full h-1/6 flex-row justify-between items-center">
+            <TouchableOpacity className="border border-black w-12 h-8"></TouchableOpacity>
+            <TouchableOpacity className="border border-black w-12 h-8"></TouchableOpacity>
+           </View>
+        </View>
+    </Background>
+   
+    )
 }
