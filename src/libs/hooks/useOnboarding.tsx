ae8cff9bb8b22d336950fe0a40dab939a79a6ab5
obb8923 <obb8923@naver.com>
2025-07-30 18:29:30 +0900

    fix: 훅 실행시 온보딩 상태를 가져와서 전역상태에 저장하는 것으로 수정

--- src/libs/hooks/useOnboarding.tsx
+++ src/libs/hooks/useOnboarding.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import { useOnboardingStore } from "../../store/onboardingStore";
 export const useOnboarding = () => {
  const {isOnboardingCompleted,isLoading,checkOnboardingStatus,completeOnboarding} = useOnboardingStore();
+ useEffect(()=>{
+  checkOnboardingStatus();
+ },[])
   return {
     isOnboardingCompleted,
     isLoading,
