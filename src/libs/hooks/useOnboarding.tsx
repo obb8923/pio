f4e2f4acfd26e852aaa3754d6d96ade8e6f34130
obb8923 <obb8923@naver.com>
2025-07-30 17:03:03 +0900

    ASYNCSTORAGE_KEYS위치수정

--- src/libs/hooks/useOnboarding.tsx
+++ src/libs/hooks/useOnboarding.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ONBOARDING_KEY } from '../../components/ASYNCSTORAGE_KEYS';
+import { ONBOARDING_KEY } from '../../constants/ASYNCSTORAGE_KEYS';
 
 export const useOnboarding = () => {
   const [isOnboardingCompleted, setIsOnboardingCompleted] = useState<boolean>(false);
