ee4c8a5098652da882e6e5bb6ffcb1afb17ca73f
obb8923 <obb8923@naver.com>
2025-09-26 17:16:57 +0900

    배경화면 그레디언트 적용

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,4 +1,5 @@
 import { Image, View, ViewStyle } from "react-native"
+import LinearGradient from "react-native-linear-gradient"
 import { TAB_BAR_HEIGHT } from "../constants/TabNavOptions"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
@@ -21,7 +22,13 @@ export const Background = ({children,isStatusBarGap=false,type='green',isTabBarG
   }
   return (
     <View className="flex-1">
-    <Image source={require('../../assets/webps/BackgroundGreen.webp')} className="w-full h-full absolute top-0 left-0 right-0 bottom-0" resizeMode="cover"/>
+      <LinearGradient
+        colors={["#67B3AD", "#A8D7AB", "#BFD97D", "#9DC75E"]}
+        locations={[0, 0.4, 0.7, 1]}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 1 }}
+        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+      />
     {type === 'white' && <View className="flex-1 mx-2 absolute top-0 left-0 right-0 bottom-0 bg-white opacity-90"/>}
     <View 
     className={`flex-1 ${props.className}`} 
