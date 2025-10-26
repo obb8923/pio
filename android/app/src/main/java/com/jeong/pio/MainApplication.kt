9c5e810dce62922a390e55d926e15aeac6e81549
obb8923 <obb8923@naver.com>
2025-10-26 21:36:56 +0900

    feat: haptic feedback

--- android/app/src/main/java/com/jeong/pio/MainApplication.kt
+++ android/app/src/main/java/com/jeong/pio/MainApplication.kt
@@ -12,7 +12,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import io.invertase.notifee.NotifeePackage
-
+import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
