eb40e05dd3e5eca782bb6170f2d71232cbea4e32
obb8923 <obb8923@naver.com>
2025-10-29 20:40:33 +0900

    feat: module-resolve도입

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,25 @@ module.exports = function(api) {
   return {
     presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
     plugins: [
+    ['module-resolver',
+    {
+      root: ['./src'], // 별칭 기준 경로
+      alias: {
+        '@': './src', // @ 를 src 폴더로 매핑
+        '@assets': './assets',
+        '@domain': './src/domain',
+        '@libs': './src/libs',
+        '@hooks': './src/hooks',
+        '@components': './src/components',
+        '@screens': './src/screens',
+        '@types': './src/types',
+        '@utils': './src/utils',
+        '@styles': './src/styles',
+        '@config': './src/config',
+        '@services': './src/services',
+        '@store': './src/store',
+      },
+    }],
       ['module:react-native-dotenv', {
         envName: 'APP_ENV',
         moduleName: '@env',
